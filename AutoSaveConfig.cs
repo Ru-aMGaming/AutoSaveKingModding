91ae3093ccf9eb32c1e91a9f50dbbecc0cd872b5
educatalan02 <34221626+educatalan02@users.noreply.github.com>
2018-11-25 21:07:56 +0100

    Add files via upload

---
+++ AutoSaveConfig.cs
@@ -0,0 +1,25 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace educatalan02.AutoSaveKingModdingNetwork
+{
+    public class AutoSaveConfig : IRocketPluginConfiguration
+
+    {
+        public float SaveInterval;
+        public string Color;
+        public string SaveMsg;
+
+        public void LoadDefaults()
+        {
+            SaveMsg = "Server saving...";
+            SaveInterval = 600f; 
+            Color = "yellow";
+        }
+    }
+}
---
+++ AutoSaveMain.cs
@@ -0,0 +1,58 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Rocket.API.Collections;
+
+namespace educatalan02.AutoSaveKingModdingNetwork
+{
+    public class AutoSaveMain : RocketPlugin<AutoSaveConfig>
+    {
+        public static AutoSaveMain Instance = null;
+
+         Func<Color> colorify = () => UnturnedChat.GetColorFromName(AutoSaveMain.Instance.Configuration.Instance.Color, Color.yellow);
+
+        protected override void Load()
+        
+        {
+
+            Instance = this;
+
+
+
+            Rocket.Core.Logging.Logger.Log("Plugin Loaded correctly. Made by educatalan02");
+            Rocket.Core.Logging.Logger.Log("If you have issues join to this discord: https://discord.gg/69C9rb6");
+
+
+            InvokeRepeating("Save", Configuration.Instance.SaveInterval, Configuration.Instance.SaveInterval);
+        }
+
+
+        protected override void Unload()
+        {
+            Rocket.Core.Logging.Logger.Log("Plugin unloaded");
+            CancelInvoke("Save");
+        }
+
+
+
+        public void Save()
+        {
+            UnturnedChat.Say(Configuration.Instance.SaveMsg, colorify());
+            Rocket.Core.Logging.Logger.Log("Saving server...!");
+            SaveManager.save();
+        
+            
+        }
+        // The ability to replace it for SaveMsg
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            {"Saving", "Saving server..."}
+        };
+    }
+}
